2e7ba5b76c180193654833f7f858016bf948a6d7
youeo <yaj5414@gmail.com>
2026-05-22 14:35:32 +0900

    edit

--- lab02/infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc" {
-  value = {
-    (local.vpc.name) = {
-      id = module.network.vpc.id
-    }
-  }
-}
-
-# trigger
--- lab02/modules/workload/datasources.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners = ["amazon"] # 공인된 아마존 이미지 만 찾도록 제한합니다.
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.*-x86_64"]
-  }
-}
